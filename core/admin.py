63b10aa471f50b05784a19d685e98cdda4ca17ee
mariaeduardanichelle <madunichelle@gmail.com>
2025-03-30 18:34:20 -0300

    feat: implementa upload de imagem de perfil para Usuários

--- core/admin.py
+++ core/admin.py
@@ -4,6 +4,7 @@ Django admin customization.
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.utils.translation import gettext_lazy as _
+from django.utils.html import format_html
 
 from core.models import User, Categoria, Tarefa
 
@@ -12,12 +13,12 @@ class UserAdmin(BaseUserAdmin):
     """Define the admin pages for users."""
 
     ordering = ["id"]
-    list_display = ["email", "name"]
+    list_display = ["email", "name", "imagem_preview"]
     fieldsets = (
         (None, {"fields": ("email", "password")}),
-        (_("Personal Info"), {"fields": ("name", "passage_id")}),
+        (_("Personal Info"), {"fields": ("name", "passage_id", "imagem")}),
         (
-            _("Permissions"),
+            _( "Permissions"),
             {
                 "fields": (
                     "is_active",
@@ -44,10 +45,17 @@ class UserAdmin(BaseUserAdmin):
                     "is_active",
                     "is_staff",
                     "is_superuser",
+                    "imagem",
                 ),
             },
         ),
     )
+    
+    def imagem_preview(self, obj):
+        if obj.imagem:
+            return format_html('<img src="{}" width="50" height="50" style="border-radius: 50%;" />', obj.imagem.url)
+        return "-"
+    imagem_preview.short_description = "Imagem"
 
 
 admin.site.register(User, UserAdmin)
