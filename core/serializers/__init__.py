9b9ff308d379075ad1756f70d869376ab5c734ac
Gabriel Alves <gabriel52.alves@gmail.com>
2025-03-30 15:25:28 -0300

    feat: adiciona serializers para os modelos Categoria e Tarefa

--- core/serializers/__init__.py
+++ core/serializers/__init__.py
@@ -1,1 +1,3 @@
 from .user import UserSerializer
+from .categoria import CategoriaSerializer, CategoriaCreateUpdateSerializer, CategoriaListSerializer
+from .tarefa import TarefaSerializer, TarefaCreateUpdateSerializer, TarefaListSerializer
