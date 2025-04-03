52b949b0160f258cdbbafeb36ca0c7f5b0253653
mariaeduardanichelle <madunichelle@gmail.com>
2025-04-03 19:30:20 -0300

    feat: acrescenta a url para registro de usuário

--- app/urls.py
+++ app/urls.py
@@ -40,6 +40,7 @@ urlpatterns = [
     # Simple JWT
     path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("api/register/", UserViewSet.as_view({"post": "register"}), name="register"),
 ]
 
 urlpatterns += static(settings.MEDIA_ENDPOINT, document_root=settings.MEDIA_ROOT)
