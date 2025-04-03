71c5ed90417c5634140fa4b880ebdf3f0da8c050
mariaeduardanichelle <madunichelle@gmail.com>
2025-04-03 19:29:36 -0300

    feat: modifca a views do user para a criação de usuário

--- core/views/user.py
+++ core/views/user.py
@@ -1,25 +1,31 @@
-from rest_framework import status
-from rest_framework.decorators import action
-from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework import status, viewsets
 from rest_framework.response import Response
-from rest_framework.viewsets import ModelViewSet
-
-from core.models import User
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.decorators import action
+from django.contrib.auth import get_user_model
 from core.serializers import UserSerializer
 
+User = get_user_model()
 
-class UserViewSet(ModelViewSet):
+class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all().order_by("id")
     serializer_class = UserSerializer
-    
+
     def get_permissions(self):
-        if self.action == 'create':
+        if self.action in ['create', 'register']:
             return [AllowAny()]
         return super().get_permissions()
 
+    @action(detail=False, methods=["post"], permission_classes=[AllowAny])
+    def register(self, request):
+        serializer = self.get_serializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.save()
+            return Response({"message": "Usuário criado com sucesso!"}, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
     @action(detail=False, methods=["get"], permission_classes=[IsAuthenticated])
     def me(self, request):
-        """Return the current authenticated user"""
         user = request.user
         serializer = UserSerializer(user)
         return Response(serializer.data, status=status.HTTP_200_OK)
